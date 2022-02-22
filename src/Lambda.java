ebeff7d19b887556c948d8c42aa200066f075c4b
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-22 12:45:54 -0600

    add commit

--- src/Lambda.java
+++ src/Lambda.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//comment
 public class Lambda {
     public static void main(String[] args) throws IOException {
         MyMethods newObj = new MyMethods();
